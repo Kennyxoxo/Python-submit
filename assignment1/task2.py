c2fc8e855ca323a093e7fc26c846c83548412a90
Kennyxoxo <108910089+Kennyxoxo@users.noreply.github.com>
2025-05-30 05:44:42 +0530

    Update task2.py

--- assignment1/task2.py
+++ assignment1/task2.py
@@ -1,10 +1,4 @@
-#Get first name
 first_name = input("Enter your first name: ")
-#Get last name
 last_name = input("Enter your last name: ")
 
-#Combine first name and last name
-full_name = first_name +" "+ last_name
-
-#Print welcome message
-print(f"Hello, {full_name}! Welcome to the Python program.")
+print(f"Hello, {first_name} {last_name}! Welcome to the Python program.")
